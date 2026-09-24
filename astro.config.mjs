c61294a226b95029957292af7eaa1e5e007bf41d
Jairo <jaiiro.cereceda@gmail.com>
2026-09-24 10:03:28 +0200

    chore: vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jairo-cereceda.github.io',
+  site: 'https://lily-pub.vercel.app/',
   vite: {
     plugins: [tailwindcss()],
     build: {
